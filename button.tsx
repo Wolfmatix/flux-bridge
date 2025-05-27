d2897a1e64d47ee5f3bcf7b564dffa9481b98e5c
Wolfmatix <92121560+Wolfmatix@users.noreply.github.com>
2025-05-28 01:27:14 +0200

    Update button.tsx

--- button.tsx
+++ button.tsx
@@ -1,6 +1,15 @@
-export function Button({ children, ...props }) {
+import { ButtonHTMLAttributes, ReactNode } from "react";
+
+type ButtonProps = {
+  children: ReactNode;
+} & ButtonHTMLAttributes<HTMLButtonElement>;
+
+export function Button({ children, ...props }: ButtonProps) {
   return (
-    <button className="px-4 py-2 rounded-xl text-white bg-blue-600 hover:bg-blue-700 transition" {...props}>
+    <button
+      className="px-4 py-2 rounded-xl text-white bg-blue-600 hover:bg-blue-700 transition"
+      {...props}
+    >
       {children}
     </button>
   );
