973e1df876af9dcb10d677689ac081537c956bb2
Wolfmatix <92121560+Wolfmatix@users.noreply.github.com>
2025-05-28 01:58:00 +0200

    Update card.tsx

--- card.tsx
+++ card.tsx
@@ -1,11 +1,13 @@
-export function Card({ children, ...props }) {
+import { ReactNode, HTMLAttributes } from "react";
+
+type CardProps = {
+  children: ReactNode;
+} & HTMLAttributes<HTMLDivElement>;
+
+export function Card({ children, ...props }: CardProps) {
   return (
     <div className="rounded-2xl border p-4 shadow-md" {...props}>
       {children}
     </div>
   );
-}
-
-export function CardContent({ children }) {
-  return <div className="p-2">{children}</div>;
 }
