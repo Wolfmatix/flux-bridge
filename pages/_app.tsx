7cc3805ea74e9d10acad5442d20d4f0ea2ec1fb5
Wolfmatix <92121560+Wolfmatix@users.noreply.github.com>
2025-05-29 01:36:38 +0200

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,26 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+// Wagmi and RainbowKit providers
+import { WagmiConfig, createConfig, configureChains } from 'wagmi'
+import { mainnet, polygon } from 'wagmi/chains'
+import { publicProvider } from 'wagmi/providers/public'
+
+const { chains, publicClient } = configureChains(
+  [mainnet, polygon],
+  [publicProvider()]
+)
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  publicClient,
+})
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <WagmiConfig config={wagmiConfig}>
+      <Component {...pageProps} />
+    </WagmiConfig>
+  )
 }
+
+export default MyApp
