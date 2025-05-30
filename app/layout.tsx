d7bef1ffac682e36adb03e4e4a061d3b9dbad723
Wolfmatix <92121560+Wolfmatix@users.noreply.github.com>
2025-05-30 14:16:03 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
+// app/layout.tsx
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-     return (
-       <html lang="en">
-         <body>{children}</body>
-       </html>
-     );
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
 }
