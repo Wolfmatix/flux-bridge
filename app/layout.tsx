f93e534729cd4fb548e04f651ed61f5b8d6b6fc5
Wolfmatix <92121560+Wolfmatix@users.noreply.github.com>
2025-05-30 14:10:23 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,7 @@
-// app/layout.tsx
-import '../styles/globals.css'; // Remove this line if you don't have a globals.css
-
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  );
+     return (
+       <html lang="en">
+         <body>{children}</body>
+       </html>
+     );
 }
